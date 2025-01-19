5c567e5129cc2d7822e43158fa8c4e7485757b26
Ryo Nakamura <upa@haeena.net>
2025-01-19 15:54:11 +0900

    mscp: use openssl@3

--- Formula/mscp.rb
+++ Formula/mscp.rb
@@ -6,13 +6,13 @@ class Mscp < Formula
   license "GPL-3.0-only"
 
   depends_on "cmake" => :build
-  depends_on "openssl@1.1" => :build
+  depends_on "openssl@3" => :build
 
   uses_from_macos "zlib"
 
   def install
     system "cmake", "-S", ".", "-B", "build",
-           "-DOPENSSL_ROOT_DIR=#{HOMEBREW_PREFIX}/opt/openssl@1.1", *std_cmake_args
+           "-DOPENSSL_ROOT_DIR=#{HOMEBREW_PREFIX}/opt/openssl@3", *std_cmake_args
     system "cmake", "--build", "build"
     bin.install "build/mscp"
     man1.install "build/mscp.1"
